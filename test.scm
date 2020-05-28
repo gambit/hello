66b65797878d79798cee4fec1236c0f94a8266af
David Wilson <david@daviwil.com>
2020-05-28 08:39:06 -0700

    It's test-equal

--- test.scm
+++ test.scm
@@ -1,17 +1,17 @@
 (define-library (github.com/gambit/hello test)
 
   (import (..))     ;; relative import of hello (preserves the version)
-  (import (_test))  ;; for test-equal? and test-error-tail
+  (import (_test))  ;; for test-equal and test-error-tail
   (import (gambit)) ;; for lambda, with-output-to-string, and
                     ;; wrong-number-of-arguments-exception?
 
   (begin
 
-    (test-equal? "hello you!\n"
-                 (with-output-to-string (lambda () (hi "you"))))
+    (test-equal "hello you!\n"
+                (with-output-to-string (lambda () (hi "you"))))
 
-    (test-equal? "bonjour hi!\n"
-                 (with-output-to-string (lambda () (salut "hi"))))
+    (test-equal "bonjour hi!\n"
+                (with-output-to-string (lambda () (salut "hi"))))
 
     (test-error-tail wrong-number-of-arguments-exception?
                      (lambda () (hi)))
